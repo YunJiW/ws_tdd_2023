e172b7b20323fef5a4b8175d43d7e5a5b7633845
YunJiW <dtroy371@gmail.com>
2023-03-06 13:54:10 +0900

    테스트스캐너에 키보드입력이아닌 문자열을 입력으로 설정

---
+++ src/test/java/com/ll/AppTests.java
@@ -0,0 +1,25 @@
+package com.ll;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class AppTests {
+
+    @Test
+    @DisplayName("스캐너에 키보드입력이 아닌 문자열을 입력으로 설정")
+    public void t1(){
+        Scanner sc= TestUtil.genScanner("안녕");
+
+        String cmd = sc.nextLine().trim();
+        Assertions.assertThat(cmd).isEqualTo("안녕");
+    }
+
+    //앱 테스트 시작
+
+
+
+}
