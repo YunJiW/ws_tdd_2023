e83a300cd5caade5caec53b8efe41e05365c0ee8
YunJiW <dtroy371@gmail.com>
2023-03-06 14:19:25 +0900

    출력을 모니터에 하지않고 문자열로 얻기

--- src/test/java/com/ll/AppTests.java
+++ src/test/java/com/ll/AppTests.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 public class AppTests {
@@ -16,6 +17,20 @@ public class AppTests {
 
         String cmd = sc.nextLine().trim();
         Assertions.assertThat(cmd).isEqualTo("안녕");
+    }
+    @Test
+    @DisplayName("출력을 모니터에 하지 않고 문자열로 얻기")
+    public void t2(){
+        //출력을 금지함
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+        System.out.print("안녕");
+        //그동안 출력되지 않던 내용들을 문자열로 반환
+        String rs = output.toString();
+        //화면 출력을 다시 한다.
+        TestUtil.clearSetOutToByteArray(output);
+
+        Assertions.assertThat(rs).isEqualTo("안녕");
+
     }
 
     //앱 테스트 시작
