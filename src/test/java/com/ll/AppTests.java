d43fcea91358d2dfb69dd9afbd4e5a5aeef26ca9
YunJiW <dtroy371@gmail.com>
2023-03-06 15:10:21 +0900

    종료기능 구현, 잘못ë 입력된 명령어 처리

--- src/test/java/com/ll/AppTests.java
+++ src/test/java/com/ll/AppTests.java
@@ -8,15 +8,18 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class AppTests {
 
+    //유틸테스트 시작
     @Test
     @DisplayName("스캐너에 키보드입력이 아닌 문자열을 입력으로 설정")
     public void t1(){
         Scanner sc= TestUtil.genScanner("안녕");
 
         String cmd = sc.nextLine().trim();
-        Assertions.assertThat(cmd).isEqualTo("안녕");
+        assertThat(cmd).isEqualTo("안녕");
     }
     @Test
     @DisplayName("출력을 모니터에 하지 않고 문자열로 얻기")
@@ -29,12 +32,45 @@ public class AppTests {
         //화면 출력을 다시 한다.
         TestUtil.clearSetOutToByteArray(output);
 
-        Assertions.assertThat(rs).isEqualTo("안녕");
+        assertThat(rs).isEqualTo("안녕");
 
     }
-
+    //유틸 테스트 끝
     //앱 테스트 시작
+    @Test
+    @DisplayName("프로그램 시작시 타이틀 출력 그리고 종료")
+    public void t3(){
+        Scanner sc= TestUtil.genScanner("종료");
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
 
+        new App(sc).run();
+
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertThat(rs)
+                .contains("== 명언 앱 ==")
+                .contains("명령)")
+                .doesNotContain("올바르지 않은 명령어입니다.")
+                .contains("프로그램이 종료되었습니다.");
+    }
+    @Test
+    @DisplayName("잘못된 명령어 입력에 대한 처리")
+    public void t4(){
+        Scanner sc= TestUtil.genScanner("""
+                종료2
+                종료
+                """.stripIndent().trim());
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        new App(sc).run();
+
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertThat(rs)
+                .contains("올바르지 않은 명령어입니다.");
+    }
 
 
 }
